4b987397528653768355ab232427df4b36edf385
Nate Smith <nwjsmith@gmail.com>
2020-06-29 17:31:18 -0400

    r lift aged brie

--- 001-gilded-rose/lib/gilded_rose.rb
+++ 001-gilded-rose/lib/gilded_rose.rb
@@ -6,40 +6,54 @@ class GildedRose
   end
 
   def update_quality
-    @items.each(&method(:applesauce))
+    @items.each do |item|
+      applesauce(item)
+    end
   end
 
   private
 
   def applesauce(item)
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality > 0
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
+    if item.name == "Aged Brie"
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
+        if item.quality < 50
+          item.quality = item.quality + 1
         end
       end
     else
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
           end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
             end
           end
         end
       end
-    end
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
         if item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
             if item.name != "Sulfuras, Hand of Ragnaros"
@@ -48,10 +62,6 @@ class GildedRose
           end
         else
           item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
         end
       end
     end
