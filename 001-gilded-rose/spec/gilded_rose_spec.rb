7b72c15ea24ed80242cda3542fbc582e6be749f2
Nate Smith <nwjsmith@gmail.com>
2020-06-29 17:09:06 -0400

    Add approvals

--- 001-gilded-rose/spec/gilded_rose_spec.rb
+++ 001-gilded-rose/spec/gilded_rose_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require_relative '../lib/gilded_rose'
 
 RSpec.describe GildedRose do
@@ -8,6 +9,17 @@ RSpec.describe GildedRose do
       GildedRose.new(items).update_quality
 
       expect(items[0].name).to eq "foo"
+    end
+
+    it "does stuff" do
+      verify do
+        items = [Item.new("Aged Brie", 0, 0)]
+        gilded_rose = GildedRose.new(items)
+
+        gilded_rose.update_quality
+
+        gilded_rose.items.map(&:to_s)
+      end
     end
   end
 end
