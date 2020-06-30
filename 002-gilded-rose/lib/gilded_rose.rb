05244c7e29c54db78a8e26ca23ed22815e2c1320
Nate Smith <nwjsmith@gmail.com>
2020-06-30 09:12:39 -0400

    r delete red

--- 002-gilded-rose/lib/gilded_rose.rb
+++ 002-gilded-rose/lib/gilded_rose.rb
@@ -15,18 +15,6 @@ class GildedRose
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
