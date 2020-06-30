1923e2d4a1ea9ec974839f0a724c1157bb7f0d3a
Nate Smith <nwjsmith@gmail.com>
2020-06-30 09:02:06 -0400

    r extract method

--- 002-gilded-rose/lib/gilded_rose.rb
+++ 002-gilded-rose/lib/gilded_rose.rb
@@ -10,6 +10,10 @@ class GildedRose
   private
 
   def update_item(item)
+    applesauce(item)
+  end
+
+  def applesauce(item)
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
         if item.name != "Sulfuras, Hand of Ragnaros"
