994e37f53cdd9b911dc38c6f3777272ddb3e13c5
Nate Smith <nwjsmith@gmail.com>
2020-06-30 09:14:08 -0400

    r delete red

--- 002-gilded-rose/lib/gilded_rose.rb
+++ 002-gilded-rose/lib/gilded_rose.rb
@@ -11,24 +11,15 @@ class GildedRose
 
   def update_item(item)
     if item.name == "Aged Brie"
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      else
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+      item.sell_in = item.sell_in - 1
+      if item.sell_in < 0
         if item.quality < 50
           item.quality = item.quality + 1
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-      nil
     else
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
