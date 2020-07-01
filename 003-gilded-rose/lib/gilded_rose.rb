dbfefaef069a93e0e147ddab1ad0892a2feeccdd
Nate Smith <nwjsmith@gmail.com>
2020-07-01 09:53:33 -0400

    empty if

--- 003-gilded-rose/lib/gilded_rose.rb
+++ 003-gilded-rose/lib/gilded_rose.rb
@@ -10,6 +10,11 @@ class GildedRose
   private
 
   def update_item(item)
+    if item.name == "Aged Brie"
+
+    else
+      
+    end
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
         if item.name != "Sulfuras, Hand of Ragnaros"
