ae34630fee62009ee4bd024079a94b3b2c94c907
Nate Smith <nwjsmith@gmail.com>
2020-07-01 10:09:24 -0400

    simplify sulfuras case

--- 003-gilded-rose/lib/gilded_rose.rb
+++ 003-gilded-rose/lib/gilded_rose.rb
@@ -41,30 +41,6 @@ class GildedRose
       end
       nil
     elsif item.name == "Sulfuras, Hand of Ragnaros"
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-          end
-        else
-        end
-      end
       nil
     else
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
