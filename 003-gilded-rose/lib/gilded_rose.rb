1bf9eec904a60c2eb8b7963919a952414eaddf1a
Nate Smith <nwjsmith@gmail.com>
2020-07-01 09:58:51 -0400

    delete dead

--- 003-gilded-rose/lib/gilded_rose.rb
+++ 003-gilded-rose/lib/gilded_rose.rb
@@ -16,11 +16,6 @@ class GildedRose
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
             if item.sell_in < 6
               if item.quality < 50
                 item.quality = item.quality + 1
