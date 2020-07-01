e06c048e7d263bc4aa21250dd6a0022b79c03465
Nate Smith <nwjsmith@gmail.com>
2020-07-01 10:04:20 -0400

    delete dead

--- 003-gilded-rose/lib/gilded_rose.rb
+++ 003-gilded-rose/lib/gilded_rose.rb
@@ -23,11 +23,6 @@ class GildedRose
       nil
     elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
       else
         if item.quality < 50
           item.quality = item.quality + 1
