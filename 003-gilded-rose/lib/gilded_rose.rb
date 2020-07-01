5198395a030d25b7e5283b04039963f3c495e595
Nate Smith <nwjsmith@gmail.com>
2020-07-01 10:10:51 -0400

    simplify if

--- 003-gilded-rose/lib/gilded_rose.rb
+++ 003-gilded-rose/lib/gilded_rose.rb
@@ -46,9 +46,7 @@ class GildedRose
       if item.quality > 0
         item.quality = item.quality - 1
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+      item.sell_in = item.sell_in - 1
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
