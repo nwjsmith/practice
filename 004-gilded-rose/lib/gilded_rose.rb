a67d0fc0c7f79c55c06becbeed052d4d79995bbd
Nate Smith <nwjsmith@gmail.com>
2020-07-12 19:39:54 -0400

    Extract method object

--- 004-gilded-rose/lib/gilded_rose.rb
+++ 004-gilded-rose/lib/gilded_rose.rb
@@ -76,25 +76,36 @@ class GildedRose
     end
   end
 
+  class BackstagePassesUpdater
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def update
+      if item.quality < 50
+        item.quality = item.quality + 1
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+      item.sell_in = item.sell_in - 1
+      if item.sell_in < 0
+        item.quality = item.quality - item.quality
+      end
+    end
+  end
 
   def update_backstage_passes(item)
-    if item.quality < 50
-      item.quality = item.quality + 1
-      if item.sell_in < 11
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
-      if item.sell_in < 6
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
-    end
-    item.sell_in = item.sell_in - 1
-    if item.sell_in < 0
-      item.quality = item.quality - item.quality
-    end
+    BackstagePassesUpdater.new(item).update
   end
 end
 
