49e8d20dbdfbac981106ab2823299cc13966e68b
Nate Smith <nwjsmith@gmail.com>
2020-07-12 19:14:13 -0400

    Simplify condition

--- 004-gilded-rose/lib/gilded_rose.rb
+++ 004-gilded-rose/lib/gilded_rose.rb
@@ -25,7 +25,7 @@ class GildedRose
       end
       nil
     else
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
