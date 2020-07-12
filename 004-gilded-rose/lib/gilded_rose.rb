65dbe5178e26afc4bf30b5a3de9f6e10559fdc51
Nate Smith <nwjsmith@gmail.com>
2020-07-12 19:37:25 -0400

    Extract method object

--- 004-gilded-rose/lib/gilded_rose.rb
+++ 004-gilded-rose/lib/gilded_rose.rb
@@ -36,16 +36,28 @@ class GildedRose
     end
   end
 
-  def update_basic_item(item)
-    if item.quality > 0
-      item.quality = item.quality - 1
+  class BasicUpdater
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
     end
-    item.sell_in = item.sell_in - 1
-    if item.sell_in < 0
+
+    def update
       if item.quality > 0
         item.quality = item.quality - 1
       end
+      item.sell_in = item.sell_in - 1
+      if item.sell_in < 0
+        if item.quality > 0
+          item.quality = item.quality - 1
+        end
+      end
     end
+  end
+
+  def update_basic_item(item)
+    BasicUpdater.new(item).update
   end
 
   def update_aged_brie(item)
