53904c957c5a92d6a340e19848f983025f0a8786
Nate Smith <nwjsmith@gmail.com>
2020-07-14 08:22:33 -0400

    Simplify if

--- 005-gilded-rose/lib/gilded_rose.rb
+++ 005-gilded-rose/lib/gilded_rose.rb
@@ -13,11 +13,8 @@ class GildedRose
 
   def update(item)
     if item.name == "Aged Brie"
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
+      if item.quality < 50
+        item.quality = item.quality + 1
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
