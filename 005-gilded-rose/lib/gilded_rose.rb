21a891e870075205bcd4ec96fe7c34008119dd69
Nate Smith <nwjsmith@gmail.com>
2020-07-14 08:36:16 -0400

    Introduced backstage passes updater

--- 005-gilded-rose/lib/gilded_rose.rb
+++ 005-gilded-rose/lib/gilded_rose.rb
@@ -71,6 +71,34 @@ class AgedBrieUpdater
   end
 end
 
+class BackstagePassesUpdater
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.sell_in < 11
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+      if item.sell_in < 6
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+    end
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0
+      item.quality = item.quality - item.quality
+    end
+  end
+end
+
 class Item
   attr_accessor :name, :sell_in, :quality
 
