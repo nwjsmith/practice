d45b085bfc052d874b3d1079640c3d42864812d5
Nate Smith <nwjsmith@gmail.com>
2020-07-14 08:05:41 -0400

    Add an approval

--- 005-gilded-rose/spec/gilded_rose_spec.rb
+++ 005-gilded-rose/spec/gilded_rose_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/gilded_rose'
+require 'approvals/rspec'
 
 RSpec.describe GildedRose do
   describe "#update_quality" do
@@ -8,6 +9,16 @@ RSpec.describe GildedRose do
       GildedRose.new(items).update_quality
 
       expect(items[0].name).to eq "foo"
+    end
+
+    it 'is characterized' do
+      verify do
+        items = [Item.new("foo", 0, 0)]
+
+        GildedRose.new(items).update_quality
+
+        items.map(&:to_s)
+      end
     end
   end
 end
