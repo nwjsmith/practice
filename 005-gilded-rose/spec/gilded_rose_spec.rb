7a63ba45c13323dee9a06028463e61345a5c7f06
Nate Smith <nwjsmith@gmail.com>
2020-07-14 08:10:48 -0400

    Full characterization

--- 005-gilded-rose/spec/gilded_rose_spec.rb
+++ 005-gilded-rose/spec/gilded_rose_spec.rb
@@ -13,7 +13,12 @@ RSpec.describe GildedRose do
 
     it 'is characterized' do
       verify do
-        items = [Item.new("foo", 0, 0)]
+        names = ["foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
+        qualities = [-1, 0, 1, 49, 50, 51]
+        sell_ins = [-1, 0, 1, 5, 6, 7, 10, 11, 12]
+        items = names.product(qualities, sell_ins).map { |name, quality, sell_in|
+          Item.new(name, quality, sell_in)
+        }
 
         GildedRose.new(items).update_quality
 
