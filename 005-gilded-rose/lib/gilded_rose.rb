d5069338891f8814c8fd03550915593f7529a1af
Nate Smith <nwjsmith@gmail.com>
2020-07-14 08:34:42 -0400

    Introduce brie update

--- 005-gilded-rose/lib/gilded_rose.rb
+++ 005-gilded-rose/lib/gilded_rose.rb
@@ -65,6 +65,18 @@ class AgedBrieUpdater
   def initialize(item)
     @item = item
   end
+
+  def update
+    if item.quality < 50
+      item.quality = item.quality + 1
+    end
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+    end
+  end
 end
 
 class Item
