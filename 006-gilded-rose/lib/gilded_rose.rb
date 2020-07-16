53d10c59e6b45cd2d4f837ba17cd7470057aff1f
Nate Smith <nwjsmith@gmail.com>
2020-07-16 09:02:31 -0400

    Extract method object

--- 006-gilded-rose/lib/gilded_rose.rb
+++ 006-gilded-rose/lib/gilded_rose.rb
@@ -75,7 +75,7 @@ class GildedRose
   class NormalUpdater
     attr_reader :item
 
-    def initialize
+    def initialize(item)
       @item = item
     end
 
@@ -101,16 +101,7 @@ class GildedRose
     elsif item.name == "Sulfuras, Hand of Ragnaros"
       SulfurasUpdater.new(item).update
     else
-      if item.quality > 0
-        item.quality = item.quality - 1
-      end
-      item.sell_in = item.sell_in - 1
-      if item.sell_in < 0
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-      end
-      nil
+      NormalUpdater.new(item).update
     end
   end
 end
