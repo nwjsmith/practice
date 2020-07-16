e46eb56ff30d2df43f388681fa32ab05f25fcc48
Nate Smith <nwjsmith@gmail.com>
2020-07-16 09:00:06 -0400

    Introduce method object

--- 006-gilded-rose/lib/gilded_rose.rb
+++ 006-gilded-rose/lib/gilded_rose.rb
@@ -60,6 +60,18 @@ class GildedRose
     end
   end
 
+  class SulfurasUpdater
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def update
+      nil
+    end
+  end
+
   def update(item)
     if item.name == "Aged Brie"
       AgedBrieUpdater.new(item).update
