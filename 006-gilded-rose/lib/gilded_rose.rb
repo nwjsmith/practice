5915fbe81f902be0cb38b4dd4a16b45838f78363
Nate Smith <nwjsmith@gmail.com>
2020-07-16 08:59:17 -0400

    Extract method object

--- 006-gilded-rose/lib/gilded_rose.rb
+++ 006-gilded-rose/lib/gilded_rose.rb
@@ -64,24 +64,7 @@ class GildedRose
     if item.name == "Aged Brie"
       AgedBrieUpdater.new(item).update
     elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.sell_in < 11
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-        if item.sell_in < 6
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-      item.sell_in = item.sell_in - 1
-      if item.sell_in < 0
-        item.quality = item.quality - item.quality
-      end
-      nil
+      BackstagePassesUpdater.new(item).update
     elsif item.name == "Sulfuras, Hand of Ragnaros"
       nil
     else
